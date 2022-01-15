a98b993c181e16b5968d80fac1771737ee40c11e
Adam Duflo <adduflo@gmail.com>
2022-01-15 14:06:03 -0500

    Initial Commit

---
+++ WhenCanTheRocksPlay/WhenCanTheRocksPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhenCanTheRocksPlayApp.swift
+//  WhenCanTheRocksPlay
+//
+//  Created by Adam Duflo on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct WhenCanTheRocksPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
