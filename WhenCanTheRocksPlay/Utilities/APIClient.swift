bc7a003d3b4ea865a9c916c303300777df56480f
Adam Duflo <adduflo@gmail.com>
2022-02-27 17:19:50 -0500

    more APIClient tweaks

--- WhenCanTheRocksPlay/Utilities/APIClient.swift
+++ WhenCanTheRocksPlay/Utilities/APIClient.swift
@@ -9,46 +9,47 @@ import Alamofire
 import Foundation
 
 protocol APIClienting {
-    ///
+    /// Returns url for host. Default implementation provided.
     static var host: String { get }
-    ///
+    /// Returns  path url terminating with `/api`. Default implementation provided.
     static var apiBase: String { get }
-    ///
+    /// Returns  path url terminating with `/v1`.  Default implementation provided.
     static var v1Base: String { get }
-    ///
+    /// Returns client access key. Default implementation provided.
     static var clientAccessKey: String { get }
-    ///
-    static var headers: HTTPHeaders { get }
+    /// Returns default headers. Default implementation provided.
+    static var defaultHeaders: HTTPHeaders { get }
     
-    ///
+    /// Returns path url terminating with `endpoint`. Default implementation provided.
+    static func url(endpoint: String) -> String
+    /// Executes get request for `/health` endpoint.
     static func getHealth() async throws -> HealthResponse
-    ///
+    /// Executes get request for `/areas` endpoint.
     static func getAreas() async throws -> AreasResponse
-    ///
+    /// Executes get request for `/areas/{id}` endpoint.
     static func getArea(id: String) async throws -> AreasByIdResponse
 }
 
 extension APIClienting {
     static var host: String { "https://whencantherocksplay.herokuapp.com" }
-    static var apiBase: String { host + "/api" }
-    static var v1Base: String { apiBase + "/v1" }
+    static var apiBase: String { host + "/api/" }
+    static var v1Base: String { apiBase + "/v1/" }
     static var clientAccessKey: String { ProcessInfo.processInfo.environment["CA_KEY"] ?? "" }
-    static var headers: HTTPHeaders { HTTPHeaders(["Access-Key": clientAccessKey]) }
+    static var defaultHeaders: HTTPHeaders { .init(["Access-Key": clientAccessKey]) }
+    
+    static func url(endpoint: String) -> String { v1Base + "/\(endpoint)" }
 }
 
 struct APIClient: APIClienting {
     static func getHealth() async throws -> HealthResponse {
-        let url = v1Base + "/health"
-        return try await AF.request(url).asyncResponseDecodable(of: HealthResponse.self)
+        return try await AF.request(url(endpoint: "health")).asyncResponseDecodable(of: HealthResponse.self)
     }
     
     static func getAreas() async throws -> AreasResponse {
-        let url = v1Base + "/areas"
-        return try await AF.request(url, headers: headers).asyncResponseDecodable(of: AreasResponse.self)
+        return try await AF.request(url(endpoint: "areas"), headers: defaultHeaders).asyncResponseDecodable(of: AreasResponse.self)
     }
     
     static func getArea(id: String) async throws -> AreasByIdResponse {
-        let url = v1Base + "areas/\(id)"
-        return try await AF.request(url, headers: headers).asyncResponseDecodable(of: AreasByIdResponse.self)
+        return try await AF.request(url(endpoint: "areas/\(id)"), headers: defaultHeaders).asyncResponseDecodable(of: AreasByIdResponse.self)
     }
 }
