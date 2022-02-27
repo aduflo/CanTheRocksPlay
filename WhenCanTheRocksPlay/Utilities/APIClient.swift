8117c4b3c729af1e941b570f0b14a40bb04a0734
Adam Duflo <adduflo@gmail.com>
2022-02-27 15:35:48 -0500

    tweak to APIClient

--- WhenCanTheRocksPlay/Utilities/APIClient.swift
+++ WhenCanTheRocksPlay/Utilities/APIClient.swift
@@ -16,7 +16,9 @@ protocol APIClienting {
     ///
     static var v1Base: String { get }
     ///
-    static var accessKey: String { get }
+    static var clientAccessKey: String { get }
+    ///
+    static var headers: HTTPHeaders { get }
     
     ///
     static func getHealth() async throws -> HealthResponse
@@ -30,7 +32,8 @@ extension APIClienting {
     static var host: String { "https://whencantherocksplay.herokuapp.com" }
     static var apiBase: String { host + "/api" }
     static var v1Base: String { apiBase + "/v1" }
-    static var accessKey: String { ProcessInfo.processInfo.environment["CA_KEY"] ?? "" }
+    static var clientAccessKey: String { ProcessInfo.processInfo.environment["CA_KEY"] ?? "" }
+    static var headers: HTTPHeaders { HTTPHeaders(["Access-Key": clientAccessKey]) }
 }
 
 struct APIClient: APIClienting {
@@ -41,12 +44,11 @@ struct APIClient: APIClienting {
     
     static func getAreas() async throws -> AreasResponse {
         let url = v1Base + "/areas"
-        let headers = HTTPHeaders(["Access-Key": accessKey])
         return try await AF.request(url, headers: headers).asyncResponseDecodable(of: AreasResponse.self)
     }
     
     static func getArea(id: String) async throws -> AreasByIdResponse {
         let url = v1Base + "areas/\(id)"
-        return try await AF.request(url).asyncResponseDecodable(of: AreasByIdResponse.self)
+        return try await AF.request(url, headers: headers).asyncResponseDecodable(of: AreasByIdResponse.self)
     }
 }
